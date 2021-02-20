9a750de0bd716d2ce8cedf206ae85b9a4600fc43
wangdana <wangdana1998@gmail.com>
2021-02-20 15:33:53 +0800

    webpack-demo

--- src/index.js
+++ src/index.js
@@ -5,5 +5,21 @@ console.log("hi2");
 
 const div = document.getElementById("App");
 div.innerHTML = `
-<img src='./assets/1.png'>
+<img src='${png}'>
 `;
+
+const button = document.createElement("button");
+button.innerText = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {
+      console.log("模块加载错误");
+    }
+  );
+};
+div.appendChild(button);
